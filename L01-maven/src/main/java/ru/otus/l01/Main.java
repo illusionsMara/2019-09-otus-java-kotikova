9bd341b6df8998c25e7425628e3a45aa6c133205
illusionsMara <iti_1610@mail.ru>
2019-10-12 15:09:01 +0300

    L01-maven  +L01-maven-jar-with-dependencies.jar (толстый jar-ник)

--- L01-maven/src/main/java/ru/otus/l01/Main.java
+++ L01-maven/src/main/java/ru/otus/l01/Main.java
@@ -2,6 +2,10 @@ package ru.otus.l01;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("Hello!");
+        String email = "client@mail.ru";
+        String phone = "7(777)777-77-77";
+
+        String clientContact = HelloOtus.getClientContact(email, phone);
+        System.out.println("clientContact: " + clientContact);
     }
 }
